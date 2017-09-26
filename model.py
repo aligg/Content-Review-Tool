d2b01d95b831a222674baf1af295d1592bc1f8a8
Ali <ali.glenesk@gmail.com>
2017-09-26 20:53:37 +0000

    Fixed post method bug from registration form

--- model.py
+++ model.py
@@ -13,9 +13,9 @@ class Item(db.Model):
     __tablename__ = "items"
 
     item_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
-    link_id = db.Column(db.String(15), nullable=True)
+    link_id = db.Column(db.String(30), nullable=True)
     body = db.Column(db.String(40000), nullable=False)
-    author = db.Column(db.String(15), nullable=True)
+    author = db.Column(db.String(30), nullable=True)
     submission = db.Column(db.String(200), nullable=True)
     subreddit = db.Column(db.String(200), nullable=True)
     permalink = db.Column(db.String(200), nullable=False)
@@ -38,6 +38,7 @@ class Reviewer(db.Model):
     reviewer_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     email = db.Column(db.String(30), nullable=False)
     handle = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
     is_manager = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
--- server.py
+++ server.py
@@ -13,6 +13,7 @@ app.secret_key = "miau"
 
 app.jinja_env.undefined = StrictUndefined
 
+
 @app.route('/')
 def index():
     """Landing Page / Queue Dashboard"""
@@ -36,6 +37,41 @@ def submit():
     #this route should: 
     #add comment submitted into db with label
     #redirect user to new batch or home for now
+
+
+    return redirect('/queue')
+
+
+@app.route('/add-reviewer')
+def new_reviewer():
+    """Display form to add a new reviewer"""
+
+    return render_template("add_reviewer.html")
+
+
+@app.route('/reg-handler', methods=["POST"])
+def add_reviewer():
+    """Processes form inputs and adds new reviewer to the db"""
+
+    email = request.form.get("email")
+    handle = request.form.get("handle")
+    password = request.form.get("password")
+    is_manager = request.form.get("is_manager")
+
+    reviewer_in_db = Reviewer.query.filter_by(email=email).all()
+
+    if reviewer_in_db == []:
+        new_reviewer = Reviewer(email=email, 
+                                handle=handle,
+                                password=password,
+                                is_manager=is_manager)
+
+        db.session.add(new_reviewer)
+        flash('New reviewer registered')
+    else: 
+        flash('This reviewer is already registered')
+
+    db.session.commit()
 
 
     return redirect('/')
